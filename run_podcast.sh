ba4be009d6c0eb6457d5463966f52876bdeee054
virgil_sisoe <sisoe24@gmail.com>
2019-03-04 16:28:04 -0500

    check git for update

--- run_podcast.sh
+++ run_podcast.sh
@@ -3,6 +3,15 @@
 # desktop app will call this script to launch the gui
 
 DIR="$( dirname ${BASH_SOURCE[0]} )"
+
+UPDATE=$( cd $DIR && git pull )
+echo $UPDATE
+if [ "$UPDATE" "==" "Already up-to-date." ]; then
+  echo "Already up to date"
+else
+  echo "Updating in progress.. thi should take a second"
+  xdg-open $DIR/CHANGELOG.RST 
+fi
 APP=$DIR/src/gui/main_gui.py
 $( which python3.7 ) $APP
 
