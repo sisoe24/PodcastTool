5cd6b2619ed8395260bda8a65f2fff5e306ad600
virgil_sisoe <sisoe24@gmail.com>
2019-03-04 16:31:42 -0500

    typo

--- run_podcast.sh
+++ run_podcast.sh
@@ -5,12 +5,11 @@
 DIR="$( dirname ${BASH_SOURCE[0]} )"
 
 UPDATE=$( cd $DIR && git pull )
-echo $UPDATE
 if [ "$UPDATE" "==" "Already up-to-date." ]; then
   echo "Already up to date"
 else
   echo "Updating in progress.. thi should take a second"
-  xdg-open $DIR/CHANGELOG.RST 
+  xdg-open $DIR/CHANGELOG.rst
 fi
 APP=$DIR/src/gui/main_gui.py
 $( which python3.7 ) $APP
