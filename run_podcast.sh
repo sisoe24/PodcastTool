ae092a01a3182fc202b770e24f47e1174452e712
virgil_sisoe <sisoe24@gmail.com>
2019-03-05 18:46:48 -0500

    check if log directory and  file exists from last session

--- run_podcast.sh
+++ run_podcast.sh
@@ -12,15 +12,21 @@ if [ "$UPDATE" "==" "Already up-to-date." ]; then
   echo "Already up to date"
 else
   echo "Update in progress.. this should take just a second"
-  xdg-open $DIR/CHANGELOG.rst
+  gedit $DIR/CHANGELOG.rst
 fi
 
 APP=$DIR/src/gui/main_gui.py
+# create log directory if doesnt already exists
+! [ -d $DIR/log ] && mkdir $DIR/log
+
 # catch fatal error that won't start the app
+# if file exists already from last session then delete
 ERROR_LOG=$DIR/log/FATAL_ERROR.txt
+[ -f $ERROR_LOG ] && rm -f $ERROR_LOG
+
 $( which python3.7 ) $APP 2> $ERROR_LOG
 
 if [ -s $ERROR_LOG ]; then
   sed -i '1s/^/FATAL ERROR\n\n' $ERROR_LOG
   gedit $ERROR_LOG
-else
+fi
