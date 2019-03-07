6c084cd84b79ba0343d1defef26ae1c70dbe5b10
virgil_sisoe <sisoe24@gmail.com>
2019-03-06 21:15:18 -0500

    gedit blocks the script for continuing

--- run_podcast.sh
+++ run_podcast.sh
@@ -12,7 +12,7 @@ if [ "$UPDATE" "==" "Already up-to-date." ]; then
   echo "Already up to date"
 else
   echo "Update in progress.. this should take just a second"
-  gedit $DIR/CHANGELOG.rst
+  xdg-open $DIR/CHANGELOG.rst
 fi
 
 APP=$DIR/src/gui/main_gui.py
