8b0cde6f11640ce4fe2a62581436b6a7bc68b509
virgil_sisoe <sisoe24@gmail.com>
2019-09-07 14:46:55 -0400

    .

--- src/podcasttool/gui/__init__.py
+++ src/podcasttool/gui/__init__.py
@@ -2,3 +2,6 @@ from .html_frame import HtmlFrame
 from .audio_frame import AudioFrame
 from .catalog_frame import CatalogFrame
 from .main_frame import MainFrame
+from .select_frame import SelectPodcast
+from .dev_frame import DevFrame
+from . import *
