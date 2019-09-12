00a6ac806d001b5dadb578aa2a77ffa21aa98176
Virgil Sisoe <virgilsisoe@Virgils-MacBook-Pro.local>
2019-09-12 12:44:54 -0400

    doc for run cli

--- src/main.py
+++ src/main.py
@@ -7,7 +7,13 @@ from podcasttool import util
 from podcasttool import gui_launch
 
 
-def run_cli(path):
+def run_cli(path, test_env=False):
+    """Run PodcastTool from command line.
+
+    Arguments:
+        path {str} - path where to parse for podcast files.
+        test_env {bool} - if True then uploads to test folder in server {default: False}.
+    """
     path = pathlib.Path(path).iterdir()
     for file in sorted(path):
         if file.name.endswith('.wav'):
@@ -16,7 +22,7 @@ def run_cli(path):
             podcast.generate_podcast()
 
     for file in podcast.files_to_upload():
-        upload_to_server(file["path"], file["server_path"], test_env=True)
+        upload_to_server(file["path"], file["server_path"], test_env)
 
     generate_html(podcast.html_page)
 
