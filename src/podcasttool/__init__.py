bfce74df08593dfd83ebd6af37ac8bd89981cf3e
virgil_sisoe <sisoe24@gmail.com>
2019-09-17 17:37:09 -0400

    check for dependecies

--- src/podcasttool/__init__.py
+++ src/podcasttool/__init__.py
@@ -1,6 +1,9 @@
 import sys
 import platform
 import subprocess
+
+from textwrap import dedent
+from datetime import datetime
 from tkinter import messagebox, TkVersion
 
 from . import logger
@@ -17,12 +20,6 @@ if TkVersion <= 8.5:
     messagebox.showinfo(message=f"your tcl-tk version {TkVersion} has some serious bugs!"
                         "please update to the last version: 8.6")
     sys.exit("tk version is old")
-
-try:
-    subprocess.check_output(["which", "ffmpeg"])
-except Exception as error:
-    messagebox.showinfo(message="ffmpeg/ffprobe doesnt appear to be installed.\nTry sudo apt install ffmpeg in terminal")
-    sys.exit("ffmpeg is not installed")
 
 
 if platform.system() == 'Darwin':
@@ -45,3 +42,39 @@ def open_log(msg, title="Error", icon="warning"):
     if user:
         log_path = util.get_path("log") / "errors.log"
         open_link(log_path)
+
+
+def write_report():
+    """Write and open log with missing dependencies info status."""
+    report_file = "log/dependecies_missing.log"
+    with open(report_file, "w") as report:
+        for app in MISSING_DEPENDECIES:
+            now = datetime.today().strftime("%x_%X")
+            msg = f"{now} - the following apps are required: {app}\n"
+            report.write(msg)
+            if OS_SYSTEM == "Linux":
+                report.write(f"type in the terminal\nsudo apt install {app}")
+            else:
+                report.write(dedent("""
+                                    ffmpeg and ffprobe are required if you
+                                    install ffmpeg manually from their website,
+                                    or you could install it with brew."""))
+    open_link(report_file)
+
+
+# Check if dependecies are installed. Launching from GUI may not show if
+# they are missing so custom msg is needed.
+DEPENDECIES = ["ffmpeg"]
+MISSING_DEPENDECIES = []
+
+if OS_SYSTEM == "Linux":
+    DEPENDECIES.append("xsel")
+for package in DEPENDECIES:
+    try:
+        subprocess.check_output(["which", package])
+    except Exception as error:
+        MISSING_DEPENDECIES.append(package)
+
+if MISSING_DEPENDECIES:
+    write_report()
+    sys.exit("some dependecies are required")
