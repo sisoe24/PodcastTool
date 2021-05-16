ba8cec3f6465c5bd7d874be1cbbffb9c1c0ad0f7
virgilsisoe <sisoe24@gmail.com>
2021-05-16 13:45:36 -0400

    added new credentials class

--- src/podcasttool/gui/__init__.py
+++ src/podcasttool/gui/__init__.py
@@ -3,5 +3,5 @@ from .audio_frame import AudioIntro
 from .catalog_frame import CatalogFrame
 from .main_frame import MainFrame
 from .select_frame import SelectPodcast
-from .menubar import MenuBar
+from .menubar import MenuBar, CredentialsEntry
 from . import *
