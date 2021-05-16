09d9dff11f126ddb127bdbb6c5ae5130a98df143
virgilsisoe <sisoe24@gmail.com>
2021-05-15 20:26:20 -0400

    updating package

--- src/podcasttool/gui/__init__.py
+++ src/podcasttool/gui/__init__.py
@@ -1,7 +1,7 @@
 from .html_frame import HtmlFrame
-from .audio_frame import AudioExport, AudioIntro
+from .audio_frame import AudioIntro
 from .catalog_frame import CatalogFrame
 from .main_frame import MainFrame
 from .select_frame import SelectPodcast
-from .dev_frame import DevFrame
+from .menubar import MenuBar
 from . import *
