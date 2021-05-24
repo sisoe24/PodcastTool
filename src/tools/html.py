3a4ced6db0d9f272d961c9bab8f131446cf4a5e2
virgilsisoe <sisoe24@gmail.com>
2021-05-24 12:58:21 -0400

    moved from yattag module

--- src/tools/html.py
+++ src/tools/html.py
@@ -4,87 +4,77 @@ import logging
 
 from datetime import datetime
 
-import yattag
-
-from startup import ARCHIVE_PATH
-from utils.util import UserConfig
+from startup import USER_ARCHIVE
+from utils import UserConfig
 
 LOGGER = logging.getLogger('podcasttool.html')
 
 
 def generate_html(html_data, test_env=False):
-    """Generate html page using yattag module.
+    settings = UserConfig()
 
-    Arguments:
-        html_data - [dict] - a dictionary with all the information
-        from the generated podcast.
-        test_env - [bool] - if True then changes path on html file to test path
-
+    credentials = f"""
+    <span class="virgil_description">Docente:</span>
+    <span class="virgil_credentials">{html_data['teacher_name']}</span>
+    <br />
+    <span class="virgil_description">Data:</span>
+    <span class="virgil_credentials">{html_data['registration_date']}</span>
+    <br />
+    <span class="virgil_description">Lezione:</span>
+    <span class="virgil_credentials">{html_data['lesson']}</span>
+    <hr />
     """
-    LOGGER.debug('generating html page info from dict: %s', html_data)
-    doc, tag, text = yattag.Doc().tagtext()
-    doc.stag('hr')
 
-    with tag('div', klass='virgil_podcast_info'):
+    body = ""
 
-        with tag('span', klass='virgil_description'):
-            text('Docente:')
-        with tag('span', klass='virgil_credentials'):
-            text(html_data['teacher_name'])
-        doc.stag('br')
+    for part_num, part_info in sorted(html_data['parts'].items()):
+        body += f"<p>{part_num} | Durata {part_info['duration']}</p>"
 
-        with tag('span', klass='virgil_description'):
-            text('Data:')
-        with tag('span', klass='virgil_credentials'):
-            text(html_data['registration_date'])
-        doc.stag('br')
+        if settings.value('html_mediaplayer', False):
+            body += f"""
+                <object id="audioplayer1" width="290" height="24" 
+                    data="{settings.value('plugin_url')}" 
+                    type="application/x-shockwave-flash">
+                        <param name="FlashVars"
+                        value="playerID=1&amp;soundFile={part_info['link']}">
+                </object>
+                """
 
-        with tag('span', klass='virgil_description'):
-            text('Lezione:')
-        with tag('span', klass='virgil_credentials'):
-            text(html_data['lesson'])
+        body += f"""
+            <a download href="{part_info['link']}" target="_blank">
+                <button class="virgil_button">Download</button>
+            </a>
+            """
 
-        doc.stag('hr')
+    page = f"""
+    <hr />
+    <div class="virgil_podcast_info">
+        {credentials}
+        <div class="virgil_podcast_part"> 
+            {body}
+        </div>
+    </div>
+    """
 
-        with tag('div', klass='virgil_podcast_part'):
-
-            for part_num, part_info in sorted(html_data['parts'].items()):
-                with tag('p'):
-                    text(f'{part_num} | Durata {part_info["duration"]}')
-
-                settings = UserConfig()
-                if settings.value('html_mediaplayer', False):
-                    with tag('object', id="audioplayer1",
-                             width="290", height="24",
-                             data=settings.value('plugin_url'),
-                             type="application/x-shockwave-flash"):
-                        doc.stag('param', name="FlashVars",
-                                 value=f"playerID=1&soundFile={part_info['link']}")
-
-                with tag('a', "download", href=part_info["link"], target='_blank'):
-                    with tag('button', klass='virgil_button'):
-                        text('Download')
-
-    def generate_archive(text):
-        """Create a html file archive for later use."""
-
-        today = datetime.today().strftime('%m.%d.%Y_%H:%M')
-        html_file = f"{today}_{html_data['archive_name']}.html"
-        file_path = os.path.join(ARCHIVE_PATH, html_file)
-        LOGGER.debug('creating html archive: %s', file_path)
-
-        with open(file_path, 'w') as file:
-            if test_env:
-                text = _test_path(text)
-            file.write(text)
-
-    page = yattag.indent(doc.getvalue())
     LOGGER.debug('html page generated')
 
     if settings.value('html_archive', False):
-        generate_archive(page)
+        _generate_archive(html_data['archive_name'], page, test_env)
 
     return page
+
+
+def _generate_archive(filename, text, test_env):
+    """Create a html file archive for later use."""
+
+    today = datetime.today().strftime('%m%d%Y_%H%M')
+    file_path = os.path.join(USER_ARCHIVE, f"{today}_{filename}.html")
+    LOGGER.debug('creating html archive: %s', file_path)
+
+    with open(file_path, 'w') as file:
+        if test_env:
+            text = _test_path(text)
+        file.write(text)
 
 
 def _test_path(text):
