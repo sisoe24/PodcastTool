a1d81bbf8d72e4e9d1ea9128bb392e35befa26b0
virgilsisoe <sisoe24@gmail.com>
2021-05-28 22:54:54 -0400

    moved logger

--- src/logger.py
+++ src/logger.py
@@ -1,15 +1,19 @@
 """Logging module for podcasttools."""
+import os
 import sys
 import logging
+import pathlib
 
-from startup import LOG_PATH
+# from startup import LOG_PATH
 
 LOGGER = logging.getLogger('podcasttool')
 
 LOGGER.setLevel(logging.DEBUG)
 
+LOG_PATH = pathlib.Path(os.path.dirname(__file__))
+LOG_PATH = os.path.join(LOG_PATH.parent, 'log')
 
-CRITICAL_LOG = logging.FileHandler(f'{LOG_PATH}/errors.log', 'a+')
+CRITICAL_LOG = logging.FileHandler(os.path.join(LOG_PATH, 'errors.log'), 'a+')
 CRITICAL_LOG.setLevel(logging.ERROR)
 CH_FORMATTER = logging.Formatter(
     '%(asctime)s [%(levelname)s] - %(module)s: %(lineno)d: %(funcName)s() - %(message)s',
@@ -17,7 +21,7 @@ CH_FORMATTER = logging.Formatter(
 CRITICAL_LOG.setFormatter(CH_FORMATTER)
 LOGGER.addHandler(CRITICAL_LOG)
 
-DEBUG_LOW = logging.FileHandler(f'{LOG_PATH}/debug.log', 'a+')
+DEBUG_LOW = logging.FileHandler(os.path.join(LOG_PATH, 'debug.log'), 'a+')
 DEBUG_LOW.setLevel(logging.DEBUG)
 FORMATTER = logging.Formatter(
     '%(asctime)s - %(filename)-20s %(funcName)-25s %(levelname)-10s %(message)s',
