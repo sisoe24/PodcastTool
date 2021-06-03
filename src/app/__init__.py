75fc3adbeb173d66472f4f2c75301e5597647392
virgilsisoe <sisoe24@gmail.com>
2021-06-03 14:04:54 -0400

    moved version into app folder

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,3 +1,4 @@
+from .version import __version__
 from .geometry import AppGeometry
 from .dialog import CustomDialog
 from .colors import Colors
--- src/app/dialog.py
+++ src/app/dialog.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
-from app.geometry import AppGeometry
+from .geometry import AppGeometry
 
 
 class CustomDialog(tk.Toplevel):
